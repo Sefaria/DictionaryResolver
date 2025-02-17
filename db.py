1747af2a1fa4de40978f78d06b5be953abf84728
Lev Israel <eliezer.israel@gmail.com>
2025-02-17 23:02:04 +0200

    feat: Remove old WordForms when determination is made

--- db.py
+++ db.py
@@ -4,19 +4,41 @@ from sefaria.model import WordForm, WordFormSet
 from models import LexRef
 from typing import Optional
 from sefaria.utils.hebrew import strip_nikkud
+from log import log
 
 def record_determination(state: dict) -> None:
     # As currently used, we shouldn't trip this, but defensive programming
     if not state["selected_association"]:
         return
 
-    existing_wordform = get_existing_wordform(state["word"], state["selected_association"])
-    if existing_wordform:
-        add_ref_to_wordform(existing_wordform, state["ref"])
+    all_wordforms = WordFormSet({"form": state["word"], "refs": state["ref"]})
+    matching_wordform = get_matching_wordform(state["word"], state["selected_association"])
+    mistaken_wordforms = [wf for wf in all_wordforms if wf != matching_wordform]
+
+    if matching_wordform:
+        add_ref_to_wordform(matching_wordform, state["ref"])
     else:
         create_wordform(state["word"], state["selected_association"], state["ref"])
 
-def get_existing_wordform(word: str, associations: list[LexRef]) -> Optional[WordForm]:
+    for mistaken_wordform in mistaken_wordforms:
+        remove_ref_from_wordform(mistaken_wordform, state["ref"])
+
+def remove_ref_from_wordform(wordform: WordForm, ref: str):
+    """
+    Remove a reference from a wordform.
+    :param word:
+    :param ref:
+    :return:
+    """
+    if getattr(wordform, "refs", None) is None:
+        return
+    elif ref not in wordform.refs:
+        return
+    else:
+        wordform.refs.remove(ref)
+    wordform.save()
+
+def get_matching_wordform(word: str, associations: list[LexRef]) -> Optional[WordForm]:
     """
     Query the database for a wordform that matches exactly the given word and associations.
     :param word:
